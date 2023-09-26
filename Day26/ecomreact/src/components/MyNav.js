abe147afbc53315b5ccced08a7968d2259709135
rob <rob_abcba@yahoo.com>
2023-09-26 11:24:13 -0700

    Convert all HTML class= to ReactJS className=

--- Day26/ecomreact/src/components/MyNav.js
+++ Day26/ecomreact/src/components/MyNav.js
@@ -1,13 +1,13 @@
 const MyNav = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-secondary navbar-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="Home.html">
+      <nav className="navbar navbar-expand-lg bg-secondary navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="Home.html">
             <img src="BuyBestLogo.PNG" alt="Logo" width="200" height="60" />
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -15,18 +15,18 @@ const MyNav = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="Home.html">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="Home.html">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-house-door"
+                    className="bi bi-house-door"
                     viewBox="0 0 16 16"
                   >
                     <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
@@ -34,14 +34,14 @@ const MyNav = () => {
                   Home
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="AboutUs.html">
+              <li className="nav-item">
+                <a className="nav-link" href="AboutUs.html">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-list-task"
+                    className="bi bi-list-task"
                     viewBox="0 0 16 16"
                   >
                     <path
@@ -57,14 +57,14 @@ const MyNav = () => {
                   About Us
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="Products.html">
+              <li className="nav-item">
+                <a className="nav-link active" href="Products.html">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-info-lg"
+                    className="bi bi-info-lg"
                     viewBox="0 0 16 16"
                   >
                     <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z" />
@@ -72,14 +72,14 @@ const MyNav = () => {
                   Products
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="FAQ.html">
+              <li className="nav-item">
+                <a className="nav-link" href="FAQ.html">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-info-lg"
+                    className="bi bi-info-lg"
                     viewBox="0 0 16 16"
                   >
                     <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z" />
@@ -87,14 +87,14 @@ const MyNav = () => {
                   FAQ
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="ContactUs.html">
+              <li className="nav-item">
+                <a className="nav-link" href="ContactUs.html">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-person-lines-fill"
+                    className="bi bi-person-lines-fill"
                     viewBox="0 0 16 16"
                   >
                     <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
@@ -103,15 +103,15 @@ const MyNav = () => {
                 </a>
               </li>
             </ul>
-            <span class="navbar-nav me-4 mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" id="btnShow1" href="#">
+            <span className="navbar-nav me-4 mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" id="btnShow1" href="#">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-box-arrow-in-right"
+                    className="bi bi-box-arrow-in-right"
                     viewBox="0 0 16 16"
                   >
                     <path
