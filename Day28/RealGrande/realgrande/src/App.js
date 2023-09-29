b27dc9339ced6b01cc9346d52588d55326d1776c
rob <rob_abcba@yahoo.com>
2023-09-28 20:02:12 -0700

    Real Estate Website Initial Commit

---
+++ Day28/RealGrande/realgrande/src/App.js
@@ -0,0 +1,38 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+// import Footer from "./components/Footer";
+import House from "./components/House";
+import { useEffect, useState } from "react";
+import SearchFilter from "./components/SearchFilter";
+
+function App() {
+  let [housesData, setHousesData] = useState([]);
+
+  // to read houses.json and send one house obj to House.js
+  useEffect(() => {
+    console.log("in useEffect");
+    const fetchData = async () => {
+      let resp = await fetch("/houses.json");
+      let data = await resp.json();
+      // console.log(' date from json - ');
+      console.log(data);
+      setHousesData(data);
+      // console.log('data from state');
+      console.log(housesData);
+      //write the data to the state so we can use it anywhere in the component
+    };
+    fetchData();
+  }, [housesData]);
+
+  return (
+    <div className="container-fluid">
+      <Header />
+      <SearchFilter houses={housesData} />
+      <House houseinfo={housesData[1]} />
+      {/* <Footer/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ Day28/RealGrande/realgrande/src/components/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <div className="row bg-warning d-flex align-items-center">
+      <div className="col-sm-3">
+        <img className="logo" src="/imgs/logo.png" alt="logo here" />
+      </div>
+      <div className="col-sm-5">
+        <p className="m-0 tagline"> Your real estate destination!</p>
+      </div>
+      <div className="col-sm-4">
+        <button className="btn btn-primary mx-3"> Login </button>
+        <button className="btn btn-success"> SignUp </button>
+      </div>
+      {/* <h1 className="bg-warning"> Header!</h1>  */}
+    </div>
+  );
+};
+export default Header;
