6c1e5b31c243125f29da5bb75f36bf36db06c68f
rob <rob_abcba@yahoo.com>
2023-09-29 19:54:33 -0700

    Surround each button with Link tags so clicking each button will navigate to its specified to= route.

--- Day28/RealGrande/realgrande/src/components/Header.js
+++ Day28/RealGrande/realgrande/src/components/Header.js
@@ -12,9 +12,16 @@ const Header = () => {
         <p className="m-0 tagline"> Your real estate destination!</p>
       </div>
       <div className="col-sm-4">
-        <button className="btn btn-primary mx-3"> Login </button>
-        <button className="btn btn-success"> SignUp </button>
+        <Link to="/login">
+          {" "}
+          <button className="btn btn-primary mx-3"> Login </button>{" "}
+        </Link>
+        <Link to="/signup">
+          {" "}
+          <button className="btn btn-success"> SignUp </button>{" "}
+        </Link>
       </div>
+
       {/* <h1 className="bg-warning"> Header!</h1>  */}
     </div>
   );
