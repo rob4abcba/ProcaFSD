3b14005b3fb24dc931e9dc60698a421c09586961
rob <rob_abcba@yahoo.com>
2023-10-11 16:46:48 -0700

    Add email and password regex pattern validation to Login.js

--- Day28/RealGrande/realgrande/src/components/Login.js
+++ Day28/RealGrande/realgrande/src/components/Login.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import Validation from "./Validation";
+
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  function handleInput(event) {
+    const newObj = { ...values, [event.target.name]: event.target.value };
+    setValues(newObj);
+  }
+
+  function handleValidation(event) {
+    event.preventDefault();
+    setErrors(Validation(values));
+  }
+
   return (
     <div className="row">
       <div className="col-sm-6 offset-3">
-        <form>
+        <form onSubmit={handleValidation}>
           <div class="mb-3">
             <label for="" class="form-label">
               Email
@@ -14,11 +33,13 @@ const Login = () => {
               class="form-control"
               placeholder=""
               aria-describedby="helpId"
+              onChange={handleInput}
             />
             <small id="helpId" class="text-muted">
               {" "}
               your email address
             </small>
+            {errors.email && <p className="text-danger">{errors.email}</p>}
           </div>
 
           <div class="mb-3">
@@ -32,10 +53,14 @@ const Login = () => {
               class="form-control"
               placeholder=""
               aria-describedby="helpId"
+              onChange={handleInput}
             />
             <small id="helpId" class="text-muted">
               Your password
             </small>
+            {errors.password && (
+              <p className="text-danger">{errors.password}</p>
+            )}
           </div>
 
           <button type="submit" class="btn btn-primary">
