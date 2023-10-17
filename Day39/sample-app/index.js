5da24946969a64ecd34241f1af68ad20e610ff05
rob <rob_abcba@yahoo.com>
2023-10-17 12:56:00 -0700

    Remove commented out code that was moved to /routes/exampleRoutes.js

--- Day39/sample-app/index.js
+++ Day39/sample-app/index.js
@@ -11,27 +11,6 @@ app.use("/", (req, res, next) => {
 
 app.use("/", exampleRoutes);
 
-// Moved app.get, post, put, and delete to separate /routes/sampleroutes.js
-// app.get("/", (req, res) => {
-//   res.send("index.js: another app.get");
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("index.js: app.get");
-// });
-
-// app.post("/", (req, res) => {
-//   res.send("index.js: app.post");
-// });
-
-// app.put("/", (req, res) => {
-//   res.send("index.js: app.put");
-// });
-
-// app.delete("/", (req, res) => {
-//   res.send("index.js: app.delete");
-// });
-
 app.listen(port, () => {
   console.log(`sample-app/index.js: Listening at http://localhost:${port}`);
 });
