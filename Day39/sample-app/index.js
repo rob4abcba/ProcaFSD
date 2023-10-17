16c01f2eca764d73f56f8a4ca8aa0703e9abc455
rob <rob_abcba@yahoo.com>
2023-10-17 15:22:18 -0700

    Use .env file to provide sensitive MongoDB username and password info.

--- Day39/sample-app/index.js
+++ Day39/sample-app/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+dotenv.config();
 
 const exampleRoutes = require("./routes/sampleroutes");
 
