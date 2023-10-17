f22d5aa1c5c415d566ac267ac3bb51b37142d9b0
rob <rob_abcba@yahoo.com>
2023-10-17 13:08:58 -0700

    Add mongoose code to connect to nagireddy MongoDB database.

--- Day39/sample-app/index.js
+++ Day39/sample-app/index.js
@@ -1,8 +1,31 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const exampleRoutes = require("./routes/sampleroutes");
+
 const app = express();
 const port = 3000;
+
+const db = (module.exports = () => {
+  try {
+    mongoose.connect(
+      "mongodb+srv://nagireddy.h1bcq2e.mongodb.net/FSDCourse?retryWrites=true&w=majority",
+      {
+        user: process.env.DBUSERNAME,
+        pass: process.env.DBPASSWORD,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("MongoDB Connection SUCCESSFUL");
+  } catch (error) {
+    console.log(error);
+    console.log("MongoDB Connection FAILED");
+  }
+});
+db();
 
 app.use("/", (req, res, next) => {
   console.log("A new request received at " + new Date(Date.now()));
