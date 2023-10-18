a093a916582481d5598c48302b5d1dd068fe2af0
rob <rob_abcba@yahoo.com>
2023-10-18 08:13:29 -0700

    Add employeeRouter.get() to studentRoutes.js. Add studentRouter.post, put, and delete.

--- Day39/sample-app/routes/studentRoutes.js
+++ Day39/sample-app/routes/studentRoutes.js
@@ -1,10 +1,15 @@
 var express = require("express");
+
+const multer = require("multer");
+let getFields = multer();
+
 var studentRouter = express.Router();
-const studentModel = require("../models/studentSchema");
+var employeeRouter = express.Router();
+const { StudentModel, EmpModel } = require("../models/studentSchema");
 
 // Get all the students data
 studentRouter.get("/", async (request, response) => {
-  const students = await studentModel.find({});
+  const students = await StudentModel.find({});
   try {
     response.send(students);
   } catch (error) {
@@ -12,17 +17,17 @@ studentRouter.get("/", async (request, response) => {
   }
 });
 
-// Get SPECIFIC student data using RollNo
+// Get the data using Roll Number
 studentRouter.get("/:RollNo", async (request, response) => {
-  const student = await studentModel.find({ RollNo: request.params.RollNo });
+  const students = await StudentModel.find({ RollNo: request.params.RollNo });
   try {
-    response.send(student);
+    response.send(students);
   } catch (error) {
     response.status(500).send(error);
   }
 });
 
-// Get GROUP of students using College
+// Get GROUP of students using College //MG: Might need to comment this section out
 studentRouter.get("/college/:College", async (request, response) => {
   const studentsByCollege = await studentModel.find({
     College: request.params.College,
@@ -34,19 +39,44 @@ studentRouter.get("/college/:College", async (request, response) => {
   }
 });
 
-// Get GROUP of students using Section
-studentRouter.get("/section/:Section", async (request, response) => {
-  const studentsBySection = await studentModel.find({
-    Section: request.params.Section,
-  });
+// Get the data using Section name
+studentRouter.get("/section/:section", async (request, response) => {
+  const students = await StudentModel.find({ Section: request.params.section });
   try {
-    response.send(studentsBySection);
+    response.send(students);
   } catch (error) {
     response.status(500).send(error);
   }
 });
 
-// Get students whose name includes substring
+//insert a student data
+studentRouter.post("/", getFields.none(), async (req, res) => {
+  try {
+    const newstudent = new StudentModel(req.body);
+    let result = await newstudent.save();
+    result = result.toObject();
+    console.log(req.body);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+//update the college name based on Roll Number
+studentRouter.put("/:RollNo", async (request, response) => {
+  const students = await StudentModel.updateOne(
+    { RollNo: { $eq: request.params.RollNo } },
+    { College: request.query.CName }
+  );
+  try {
+    response.send(students);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+/ Get students whose name includes substring //MG: Might need to comment this out
 studentRouter.get("/name/:Substring", async (request, response) => {
   const studentsByName = await studentModel.find({
     Name: {
@@ -61,4 +91,25 @@ studentRouter.get("/name/:Substring", async (request, response) => {
   }
 });
 
-module.exports = studentRouter;
+//Delete student using Roll Number
+app.delete("/:RollNo", async (request, response) => {
+  const students = await StudentModel.deleteOne({
+    RollNo: request.params.RollNo,
+  });
+  try {
+    response.send(students);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+// Get all the data from EmployeewithExp collection
+employeeRouter.get("/", async (request, response) => {
+  const empldata = await EmpModel.find({});
+  try {
+    response.send(empldata);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+module.exports = { studentRouter, employeeRouter };
